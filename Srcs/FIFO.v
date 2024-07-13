418f7dc58e9b0df70db1c256a3f29fc4b1d060ab
Vedhant abrol <66167443+vedhant007@users.noreply.github.com>
2024-07-13 11:48:17 +0530

    Update FIFO.v
    adding wblock with binary counter. will add gray style counter since I simulate whole FIFO .

--- Srcs/FIFO.v
+++ Srcs/FIFO.v
@@ -10,3 +10,25 @@ module FIFOmem(input [7:0]wdata,input wclken,wclk,wfull,input [22:0]waddr,raddr,
   
 endmodule
 
+module FIFO_wblock(input winc,wclk,wrst_n,output  [22:0]waddr,output  [23:0]wptr,wq2_rptr,output reg wfull);//wptr has one extra bit for checking full and empty condition
+
+
+reg [23:0]wbin;
+wire wfull_val;
+always @(posedge wclk or negedge wrst_n)begin
+if(!wrst_n)begin
+wbin<=23'b0;
+end
+else if (winc & ~wfull)begin
+wbin<=wbin+1; 
+end
+end
+assign waddr=wbin[22:0];
+assign wptr=wbin; 
+assign wfull_val={~wptr[23],wptr[22:0]}^wq2_rptr; // full condition
+
+always @(posedge wclk or negedge wrst_n) begin
+if(!wrst_n) wfull<=1'b0;
+else wfull<= wfull_val;
+end
+endmodule
