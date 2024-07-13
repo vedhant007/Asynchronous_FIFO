426a915631b5672883a98870c99cfb2cfd53e1a0
Vedhant abrol <66167443+vedhant007@users.noreply.github.com>
2024-07-13 12:09:37 +0530

    Update FIFO.v
    fixed wfull condition, added wbin_next as it is considered good practice in sequential circuit design and also corrected wbin size to 24 bits.

--- Srcs/FIFO.v
+++ Srcs/FIFO.v
@@ -14,18 +14,20 @@ module FIFO_wblock(input winc,wclk,wrst_n,output  [22:0]waddr,input [23:0]wq2_rp
 
 
 reg [23:0]wbin;
+ wire [23:0] wbin_next;
 wire wfull_val;
 always @(posedge wclk or negedge wrst_n)begin
 if(!wrst_n)begin
-wbin<=23'b0;
+wbin<=24'b0;
 end
 else if (winc & ~wfull)begin
-wbin<=wbin+1; 
+wbin<=wbin_next; 
 end
 end
+assign wbin_next=wbin+1;
 assign waddr=wbin[22:0];
 assign wptr=wbin; 
-assign wfull_val={~wptr[23],wptr[22:0]}^wq2_rptr; // full condition
+assign wfull_val=(wbin_next[23]!=wq2_rptr)&&(wbin_nect[22:0]==wq2_rptr[22:0]); // full condition
 
 always @(posedge wclk or negedge wrst_n) begin
 if(!wrst_n) wfull<=1'b0;
